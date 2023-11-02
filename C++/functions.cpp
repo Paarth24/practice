04788f563904f078d92dfed3d3be7507ee6ce96e
Paarth24 <paarth.pugalia1102@gmail.com>
2023-11-02 09:37:30 +0530

    learned exceptions

--- C++/functions.cpp
+++ C++/functions.cpp
@@ -3,7 +3,16 @@ using namespace std;
 #include "test_functions.h"
 
 int main(){
-    int a{10};
-    a = max(&a);
-    cout << a << endl;
+    int a,b;
+    cout << "Enter Divedend" << endl;
+    cin >> a;
+    cout << "Enter Divisor" << endl;
+    cin >> b;
+    try{
+        cout << divide(a,b);
+    }catch(int x){
+        cerr << "Error" << endl;
+    }
+
+    cout << "Continue test" << endl;
 }
--- C++/test_functions.h
+++ C++/test_functions.h
@@ -1,5 +1,13 @@
-int max(int* a){
+int divide(int a, int b){
+    if(b == 0){
+        throw 10;
+    }else{
+        return(a/b);
+    }
+}
+
+int max(int a){
     return(
-        *a + 100
+        a + 100
     );
 }
